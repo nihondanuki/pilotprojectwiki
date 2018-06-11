0d7865d48b9aa4e72f893a79c0ede343b4f1148b
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-11 15:36:22 +0900

    パスワードにバリデーション

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe '#name' do
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_length_of(:name).
-      is_at_most(50)
-    }
+    it { is_expected.to validate_length_of(:name).is_at_most(50) }
   end
   
   describe '#email' do
     it { is_expected.to validate_presence_of(:email)}
-    it { is_expected.to validate_length_of(:email).
-      is_at_most(50)
-    }
+    it { is_expected.to validate_length_of(:email).is_at_most(50) }
     it { is_expected.not_to allow_value("invalid").for(:email)}
     it { is_expected.to allow_value("a@a.com").for(:email)}
     it { is_expected.to validate_uniqueness_of(:email) }
@@ -20,5 +16,6 @@ RSpec.describe User, type: :model do
 
   describe '#password' do
     it { is_expected.to have_secure_password }
+    it { is_expected.to validate_length_of(:password).is_at_least(9) }
   end
 end
