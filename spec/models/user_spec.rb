3d0d3b1ed1d19283a8a91fb0185fe487c4385a06
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-11 12:39:16 +0900

    add password to users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#name' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).
+      is_at_most(50)
+    }
+  end
+  
+  describe '#email' do
+    it { is_expected.to validate_presence_of(:email)}
+    it { is_expected.to validate_length_of(:email).
+      is_at_most(50)
+    }
+    it { is_expected.not_to allow_value("invalid").for(:email)}
+    it { is_expected.to allow_value("a@a.com").for(:email)}
+    it { is_expected.to validate_uniqueness_of(:email) }
+  end
+
+  describe '#password' do
+    it { is_expected.to have_secure_password }
+  end
+end
