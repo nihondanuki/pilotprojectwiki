f2e25859f624b71599af13503fd33e5eabe3b32c
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-15 13:14:20 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-  get '/login' to: 'sessions#new'
-  post '/login' to: 'sessions#create'
-  delete '/logout' to: 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
 end
