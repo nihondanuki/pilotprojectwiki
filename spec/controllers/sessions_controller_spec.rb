deef0718bf1f57ea5e9eb3c7eaaed3b218338678
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-15 14:42:27 +0900

    session #newのテストを追加

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,10 +3,25 @@ require 'rails_helper'
 RSpec.describe SessionsController, type: :controller do
 
   describe "GET #new" do
-    it "returns http success" do
+    before do
       get :new
-      expect(response).to have_http_status(:success)
     end
+
+    it "returns http success" do
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST #create" do
+    context "valid" do
+    end
+
+    context "invalid" do
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it
   end
 
 end
