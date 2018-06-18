ce169436f092539b6f42304b88e7c0d6ca9a40c8
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-18 12:54:55 +0900

    セッションコントローラーのテストを修正

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -12,17 +12,24 @@ RSpec.describe SessionsController, type: :controller do
 
   describe "POST #create" do
     context "valid" do
+      let!(:user){create(:user)}
       before do
-        post :create,
+        post :create, params: { session: { email: user.email, password: user.password }}
       end
+      it { expect(response).to redirect_to user }
+      it { is_expected.to be_logged_in }
     end
 
     context "invalid" do
+      let!(:user){create(:user)}
+      before do
+        post :create, params: { session: { email: "foo", password: user.password }}
+      end
+      it { expect(response).to render_template "new" }
+      it { is_expected.not_to be_logged_in }
     end
   end
 
-  describe "DELETE #destroy" do
-    it
-  end
+  describe "DELETE #destroy"
 
 end
