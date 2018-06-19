5c756feb73544009e1857d70d948a605a73c3963
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-19 17:27:53 +0900

    テストを追加

--- spec/models/inquiry_spec.rb
+++ spec/models/inquiry_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Inquiry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#user_id' do
+    it { is_expected.to validate_presence_of(:user_id)}
+  end
+
+  describe '#body' do
+    it { is_expected.to validate_presence_of(:body)}
+    it { is_expected.to validate_length_of(:name).is_at_most(30) }
+  end
+
+  describe '#subject' do
+    it { is_expected.to validate_presence_of(:subject)}
+    it { is_expected.to validate_length_of(:name).is_at_most(1000) }
+  end
+  
 end
