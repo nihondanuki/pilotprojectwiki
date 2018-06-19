62db53c95ebe5647c21ae090ccbbb98b4f92f3b8
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-19 17:26:15 +0900

    逆順に変更

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,5 +1,6 @@
 class Inquiry < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presense: true
   validates :subject, presense: true, length: { maximum: 30 }
   validates :body, presense: true, length: { maximum: 1000 }
