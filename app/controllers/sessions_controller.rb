bac9b8ce4d2ac1712735ffc69306724151b7b957
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-19 13:24:09 +0900

    ミス

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: session_params[:email])
-    if user&.authenticate(session_params[:password])
+    if user&.authenticate(session_params[:password])a
       log_in user
       redirect_to user
     else
