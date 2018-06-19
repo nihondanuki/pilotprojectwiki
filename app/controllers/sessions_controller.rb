fac3a163aee0291c11abe9e3f7cee661a0b34c51
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-19 13:23:22 +0900

    コントローラーのログイン認証の変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: session_params[:email])
-    if user && user.authenticate(session_params[:password])
+    if user&.authenticate(session_params[:password])
       log_in user
       redirect_to user
     else
