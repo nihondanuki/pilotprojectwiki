38fbe92a5d355aceebacca46ce24303148779065
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-19 16:50:40 +0900

    validationを追加

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,3 +1,6 @@
 class Inquiry < ApplicationRecord
   belongs_to :user
+  validates :user_id, presense: true
+  validates :subject, presense: true, length: { maximum: 30 }
+  validates :body, presense: true, length: { maximum: 1000 }
 end
