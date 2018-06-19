bbc6668dad050c510b2e3fa4fab880fac3d13932
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-19 15:29:31 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180611032045) do
+ActiveRecord::Schema.define(version: 20180619061733) do
+
+  create_table "inquiries", force: :cascade do |t|
+    t.text "body"
+    t.string "subject"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_inquiries_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_inquiries_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
