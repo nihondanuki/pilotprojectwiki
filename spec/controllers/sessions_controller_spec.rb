20584b46444d85539891285f3917548f61c590d6
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-19 13:55:29 +0900

    fix コントローラーのテスト
    helper specを修正

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,41 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  subject{response}
   describe "GET #new" do
     before { get :new }
 
-    it { expect(response).to be_successful }
+    it { is_expected.to be_successful }
   end
 
   describe "POST #create" do
     context "login params is valid" do
       let(:user){create(:user)}
       before { post :create, params: { session: { email: user.email, password: user.password }} }
-      it { expect(response).to redirect_to user }
+      it { is_expected.to redirect_to user }
     end
 
     context "login params is invalid" do
       let(:user){create(:user)}
       context "email is invalid" do
         before { post :create, params: { session: { email: "foo", password: user.password }} }
-        it { expect(response).to render_template "new" }
+        it { is_expected.to render_template "new" }
       end
 
       context "password is invalid" do
         before { post :create, params: { session: { email: user.email, password: "foo" }} }
-        it { expect(response).to render_template "new" }
+        it { is_expected.to render_template "new" }
       end
 
       context "email is nil" do
         before { post :create, params: { session: { email: nil, password: user.password }} }
-        it { expect(response).to render_template "new" }
+        it { is_expected.to render_template "new" }
       end
     end
   end
 
   describe "DELETE #destroy" do
     before { delete :destroy }
-    it { expect(response).to render_template "new" }
+    it { is_expected.to render_template "new" }
   end
 
 end
--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -2,39 +2,36 @@ require 'rails_helper'
 
 RSpec.describe SessionsHelper, type: :helper do
   describe "#logged_in?" do
-    context do
+    subject{logged_in?}
+    context "ログインしているとき" do
       let!(:current_user){create(:user)}
-      it { expect(logged_in?).to eq true}
+      it { is_expected.to eq true}
     end
 
-    context do
+    context "ログインしているとき" do
       let!(:current_user){nil}
-      it { expect(logged_in?).to eq false}
+      it { is_expected.to eq false}
     end
   end
 
   describe "#log_in" do
-    context do
-      let!(:user){create(:user)}
-      before do
-        log_in user
-      end
-      it { expect(session[:user_id]).to eq user.id }
+    let(:user){create(:user)}
+    before do
+      log_in user
     end
+    it { expect(session[:user_id]).to eq user.id }
   end
 
   describe "#current_user" do
-    context do
-      let!(:user){create(:user)}
-      before do
-        session[:user_id] = user.id
-      end
-      it { expect(current_user).to eq user }
+    let(:user){create(:user)}
+    before do
+      session[:user_id] = user.id
     end
+    it { expect(current_user).to eq user }
   end
 
   describe "#log_out" do
-    let!(:user){create(:user)}
+    let(:user){create(:user)}
     before do
       session[:user_id] = user.id
       current_user
