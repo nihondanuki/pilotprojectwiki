f96739385c4b52f797df6fbc0b7465d1590ec426
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-21 10:33:53 +0900

    ルーティングファイルに記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :users
+  resources :inquiries
 
 end
