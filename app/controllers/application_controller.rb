18173275f7ee016cd4d759a8bdff27a7de7594ac
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-21 12:11:27 +0900

    ログイン状態を取得するメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+
+    def login_watch
+      unless logged_in?
+        flash.now[:danger] = "ログインが必要です"
+        redirect_to login_url
+      end
+    end
 end
