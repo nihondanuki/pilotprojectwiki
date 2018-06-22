718cfa2a8a9d3fda708d108ee72d8594c023b9b7
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-22 14:19:08 +0900

    コントローラーでajaxを使わないため削除

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -3,13 +3,11 @@ class InquiriesController < ApplicationController
   before_action :login_watch, only: [:new, :edit, :create, :update, :destroy]
 
   # GET /inquiries
-  # GET /inquiries.json
   def index
     @inquiries = Inquiry.all
   end
 
   # GET /inquiries/1
-  # GET /inquiries/1.json
   def show
   end
 
@@ -23,40 +21,29 @@ class InquiriesController < ApplicationController
   end
 
   # POST /inquiries
-  # POST /inquiries.json
   def create
     @inquiry = current_user.inquiries.build(inquiry_params)
 
-    respond_to do |format|
-      if @inquiry.save
-        format.html { redirect_to @inquiry, notice: 'Inquiry was successfully created.' }
-      else
-        format.html { render :new }
-      end
+    if @inquiry.save
+      redirect_to @inquiry
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /inquiries/1
-  # PATCH/PUT /inquiries/1.json
   def update
-    respond_to do |format|
-      if @inquiry.update(inquiry_params)
-        format.html { redirect_to @inquiry, notice: 'Inquiry was successfully updated.' }
-        format.json { render :show, status: :ok, location: @inquiry }
-      else
-        format.html { render :edit }
-        format.json { render json: @inquiry.errors, status: :unprocessable_entity }
-      end
+    if @inquiry.update(inquiry_params)
+      redirect_to @inquiry
+    else
+      render :edit
     end
   end
 
   # DELETE /inquiries/1
-  # DELETE /inquiries/1.json
   def destroy
     @inquiry.destroy
-    respond_to do |format|
-      format.html { redirect_to inquiries_url, notice: 'Inquiry was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to inquiries_url
     end
   end
 
