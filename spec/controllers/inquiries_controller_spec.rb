0631cdba38f30d34f0fe0ac7671489b043b44227
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-25 19:02:15 +0900

    テスト作成

--- spec/controllers/inquiries_controller_spec.rb
+++ spec/controllers/inquiries_controller_spec.rb
@@ -18,6 +18,35 @@ RSpec.describe InquiriesController, type: :controller do
     end
   end
 
+  describe "GET #edit" do
+    context "ログイン中" do
+      context "ユーザーが質問の作成者である" do
+        let(:inquiry) { create(:inquiry) }
+        before do
+          log_in inquiry.user
+          get :edit, params: { id: inquiry.id }
+        end
+        it { expect(response).to have_http_status :ok }
+      end
+
+      context "ユーザーは作成者じゃない場合" do
+        let(:inquiry) { create(:inquiry) }
+        let(:invalid_user) { User.create(attributes_for(:user, email: "invalid@x.com"))}
+        before do
+          log_in invalid_user
+          get :edit, params: { id: inquiry.id }
+        end
+        it { is_expected.to redirect_to inquiry }
+      end
+    end
+
+    context "ログアウト状態" do
+      let(:inquiry) { create(:inquiry) }
+      before { get :edit, params: { id: inquiry.id } }
+      it { is_expected.to redirect_to login_path }
+    end
+  end
+
   describe "POST #create" do
     context "ログイン時" do
       let!(:user) { create(:user) }
@@ -39,6 +68,29 @@ RSpec.describe InquiriesController, type: :controller do
     context "ログアウト状態" do
       let(:inquiry_params) { attributes_for(:inquiry) }
       subject{ post :create, params: { inquiry: inquiry_params } }
+      it { is_expected.to redirect_to login_path }
+    end
+  end
+
+  describe "PATCH #update" do
+    context "ログイン時" do
+      context "ユーザーが質問の作成者である" do
+        let(:inquiry) { create(:inquiry) }
+        before { log_in inquiry.user }
+        context "投稿がvalid" do
+          before { patch :update, params: { id: inquiry.id, inquiry: attributes_for(:inquiry, body: "body") } }
+          it { is_expected.to redirect_to inquiry }
+        end
+        context "投稿がinvalid" do
+          before { patch :update, params: { id: inquiry.id, inquiry: attributes_for(:inquiry, body: "a"*1002) } }
+          it { is_expected.to render_template "edit" }
+        end
+      end
+    end
+
+    context "ログアウト状態" do
+      let(:inquiry) { create(:inquiry) }
+      subject{ patch :update, params: { id: inquiry.id, inquiry: attributes_for(:inquiry, body: "body") } }
       it { is_expected.to redirect_to login_path }
     end
   end
