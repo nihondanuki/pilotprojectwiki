69332e5a823e99699fa012be38e78ea8475c9ecc
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-25 17:16:29 +0900

    編集できるようにする

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,10 +1,14 @@
 class InquiriesController < ApplicationController
   before_action :set_inquiry, only: [:show, :edit, :update, :destroy]
   before_action :require_login, only: [:new, :edit, :create, :update, :destroy]
+  before_action :require_permit, only: [:edit, :update, :destroy]
 
   # GET /inquiries/new
   def new
     @inquiry = current_user.inquiries.build
+  end
+
+  def edit
   end
 
   # POST /inquiries
@@ -18,6 +22,14 @@ class InquiriesController < ApplicationController
     end
   end
 
+  def update
+    if @inquiry.update(inquiry_params)
+      redirect_to @inquiry
+    else
+      render :edit
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_inquiry
@@ -27,5 +39,11 @@ class InquiriesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def inquiry_params
       params.require(:inquiry).permit(:subject, :body)
+    end
+
+    def require_permit
+      unless (current_user == @inquiry.user)
+        redirect_to @inquiry
+      end
     end
 end
