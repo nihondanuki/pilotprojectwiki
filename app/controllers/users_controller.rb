6ae46e12e4783b4dc017e6839957283932e9a52b
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-27 16:53:03 +0900

    テスト 変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        log_in @user
-        redirect_to inquiries_path
-      else
-        render :new
-      end
+    if @user.save
+      log_in @user
+      redirect_to inquiries_path
+    else
+      render :new
     end
   end
 
