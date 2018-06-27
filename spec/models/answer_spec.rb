872b36873ffd34eda38401871d2e76a1ef25510f
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-27 18:30:28 +0900

    answermodeltest

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'userとの関連' do
+    it { is_expected.to belong_to(:user)}
+  end
+
+  describe 'inquiryとの関連' do
+    it { is_expected.to belong_to(:user)}
+  end
+
+  describe '#validate' do
+    context 'user_id' do
+      it { is_expected.to validate_presence_of(:user_id)}
+    end
+
+    context 'inquiry_id' do
+      it { is_expected.to validate_presence_of(:inquiry_id)}
+    end
+
+    context '#content' do
+      it { is_expected.to validate_presence_of(:content)}
+      it { is_expected.to validate_length_of(:content).is_at_most(1000) }
+    end
+  end
 end
