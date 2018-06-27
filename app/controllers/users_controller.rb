19b8b8a352f97263bcad09bbddfbba06fded37c6
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-27 16:34:22 +0900

    リダイレクト先とログインの仕様を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,11 +29,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        log_in @user
+        format.html { redirect_to inquiries_path, notice: 'User was successfully created.' }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
