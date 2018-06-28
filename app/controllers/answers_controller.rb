d9458eb4f3e72d98f96eeff3071c837eab5b5101
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-28 13:39:29 +0900

    ログイン

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,6 @@
 class AnswersController < ApplicationController
+  before_action :require_login, only: [:create]
+
   def create
     inquiry ||= Inquiry.find(answer_params[:inquiry])
     @answer = inquiry.answers.create(content: answer_params[:content], user_id: current_user.id)
