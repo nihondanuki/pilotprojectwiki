d85711512ac1df9146e2838f3b8cb8cadaa18de1
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-28 13:40:58 +0900

    テスト

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  describe "POST #create" do
+    context "ログインしているとき" do
+      subject { post :create, params: { answer: { inquiry: inquiry, content: answer.content } } }
+      let(:user) { create(:user) }
+      let(:inquiry) { create(:inquiry, user: user ) }
+      let(:answer) { create(:answer, user: user, inquiry: inquiry) }
+      it do
+        binding.pry
+      end
+      before { log_in user }
+      it { is_expected.to redirect_to inquiry }
+    end
 
-  describe "GET #create" do
-    it "returns http success" do
-      let(:inquiry) { create(:inquiry) }
-      get :create
-      expect(response).to redirect_to inquiry
+    context "ログインしていないとき" do
+      subject { post :create, params: { answer: { } } }
+      let(:user) { create(:user) }
+      let(:inquiry) { create(:inquiry, user: user ) }
+      let(:answer) { create(:answer, user: user, inquiry: inquiry) }
+      it { is_expected.to redirect_to login_path }
     end
   end
-
 end
